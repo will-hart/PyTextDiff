f7f3d2676e1a78311c9e7c0b654478d48ad676a9
William Hart <hart.wl@gmail.com>
2012-08-30 22:25:28 +0100

    Improve accuracy for initial elements

--- PyFreeDiff.py
+++ PyFreeDiff.py
@@ -4,7 +4,7 @@ Based on Python Difflib and available under the ultra permissive MIT license
 
 
 Version 0.1 - 28-08-2012
-		- initial release, splitting of sentences and diff object/html diff generation
+        - initial release, splitting of sentences and diff object/html diff generation
 
 
 The MIT License (MIT)
@@ -51,16 +51,16 @@ STANDARD_REGEX = '[.!?]'
 A container for diff results
 '''
 class DiffResult():
-	start_index = 0
-	length = 0
-	line = None
-	operation = 0
+    start_index = 0
+    length = 0
+    line = None
+    operation = 0
 
-	def __init__(self, start_index, length, line, operation):
-		self.start_index = start_index
-		self.length = length
-		self.line = line
-		self.operation = operation
+    def __init__(self, start_index, length, line, operation):
+        self.start_index = start_index
+        self.length = length
+        self.line = line
+        self.operation = operation
 
 
 '''
@@ -68,173 +68,184 @@ A class for performing diff and patch operations on strings
 '''
 class DiffEngine(object):
 
-	'''
-	Performs a basic diff on two strings, splitting by sentence and returning
-	the diffs between original and revised
-	'''
-	def diff(self, original, revised):
-		results = []
-		
-		# build a list of sentences for each input string
-		original_arr = self._split_with_maintain(original)
-		new_arr = self._split_with_maintain(revised)
-		
-		# diff the result
-		raw_diffs = list(difflib.ndiff(original_arr, new_arr))
-		
-		# convert to diff format
-		for i,d in enumerate(raw_diffs):
-			if d[0] == '-':
-				op = DELETED
-			elif d[0] == '+':
-				op = INSERTED
-			else:
-				op = UNCHANGED
-			results.append(DiffResult(i, 1, d[2:], op))
-		
-		return results
-		
-	'''
-	Generates a 3 way diff between three related documents, allowing rebasing of 'mine'
-	based on changes from 'theirs'
-	'''
-	def diff3(self, mine, original, theirs):
-		result = []
-	
-		# build a hash map for each string
-		mine_arr = self._split_with_maintain(mine)
-		original_arr = self._split_with_maintain(original)
-		their_arr = self._split_with_maintain(theirs)
-		
-		# get patches between theirs and original
-		their_updates = self.diff(hashed_original, hashed_theirs)
-		your_updates = self.diff(hashed_original, hashed_mine)
-		
-		# now we need to check if there are overlapping diffs
-		pass
+    '''
+    Performs a basic diff on two strings, splitting by sentence and returning
+    the diffs between original and revised
+    '''
+    def diff(self, original, revised):
+        
+        # build a list of sentences for each input string
+        original_arr = self._split_with_maintain(original)
+        new_arr = self._split_with_maintain(revised)
+        
+        # diff the result
+        raw_diffs = list(difflib.ndiff(original_arr, new_arr))
+        
+        return self._pack_results(raw_diffs)
+    
+    '''
+    Generates a 3 way diff between three related documents, allowing rebasing of 'mine'
+    based on changes from 'theirs'
+    '''
+    def diff3(self, mine, original, theirs):
+        result = []
+    
+        # build a hash map for each string
+        mine_arr = self._split_with_maintain(mine)
+        original_arr = self._split_with_maintain(original)
+        their_arr = self._split_with_maintain(theirs)
+        
+        # get patches between theirs and original
+        their_updates = self.diff(hashed_original, hashed_theirs)
+        your_updates = self.diff(hashed_original, hashed_mine)
+        
+        # now we need to check if there are overlapping diffs
+        pass
 
-	'''
-	Splits a string based on a list of chars and returns the list.
-	For instance, when calling split_with_maintain(str, '[.!?]')
-		"This is a test.  It should split awesomely."
-	Becomes:
-		["This is a test.", "  ", "It should split awesomely"]
-	'''
-	def _split_with_maintain(self, value, treat_trailing_spaces_as_sentence = True, split_char_regex = STANDARD_REGEX):
-		result = []
-		check = value
-		
-		# compile regex
-		rx = re.compile(split_char_regex)
-		
-		# traverse the string
-		while len(check) > 0:
-			found  = rx.search(str(check))
-			if found == None:
-				result.append(check)
-				break
-			
-			idx = found.start()
-			result.append(str(check[:idx]))			# append the string
-			result.append(str(check[idx:idx+1]))	# append the puncutation so changing ? to . doesn't invalidate the whole sentence
-			check = check[idx + 1:]
-			
-			# group the trailing spaces if requested
-			if treat_trailing_spaces_as_sentence:
-				space_idx = 0
-				while True:
-					if space_idx >= len(check):
-						break
-					if check[space_idx] != " ":
-						break
-					space_idx += 1
-				
-				if space_idx != 0:
-					result.append(check[0:space_idx])
-			
-				check = check[space_idx:]
-			
-		return result
+        
+    '''
+    Generate basic HTML for a set of diffs
+    '''
+    def generate_html_diffs(self, diffs):
+        if diffs == None:
+            return ""
+
+        start = ""
+        end = ""
+        result = ""
+        
+        for diff in diffs:
+            op = diff.operation
+            if op == INSERTED:
+                start = "<ins>"
+                end = "</ins>"
+            elif op == DELETED:
+                start = "<del>"
+                end = "</del>"
+            else:
+                start = ""
+                end = ""
+            result += start + diff.line + end + "\n"
+            
+        return result
+
+    
+    '''
+    Apply a set of diffs to an original file to produce a new text file
+    '''
+    def apply_patch(self, doc, diffs):
+        if diffs == None:
+            return doc
+
+        splits = self._split_with_maintain(doc)
+        result = []
+        lastIndex = 1
+
+        for d in diffs:
+            # bring the original document up to speed
+            if d.start_index > lastIndex:
+                result += splits[lastIndex:d.start_index]
+            lastIndex += 1
+
+            # respond to the diff
+            if d.operation == DELETED:
+                lastIndex += d.length
+            elif d.operation == INSERTED:
+                result += d.line
+            else:
+                result += d.line
+                lastIndex += d.length
+
+        return ''.join(result)
+
+    '''
+    Reverse the direction of a patch
+    '''
+    def switch_patch_direction(self, diff):
+        result = []
+        for d in diff:
+            if d.operation == INSERTED:
+                new_op = DELETED
+            elif d.operation == DELETED:
+                new_op = INSERTED
+            else:
+                new_op = UNCHANGED
+
+            d.operation = new_op
+            result.append(DiffResult(d.start_index, d.length, d.line, new_op))
+        
+        return result
+
+    '''
+    Convenience method for removing a patch
+    '''
+    def remove_patch(self, doc, diffs):
+        patches = self.switch_patch_direction(diffs)
+        return self.apply_patch(doc, patches)
 
 
-		
-	'''
-	Generate basic HTML for a set of diffs
-	'''
-	def generate_html_diffs(self, diffs):
-		if diffs == None:
-			return ""
+    '''
+    Splits a string based on a list of chars and returns the list.
+    For instance, when calling split_with_maintain(str, '[.!?]')
+        "This is a test.  It should split awesomely."
+    Becomes:
+        ["This is a test.", "  ", "It should split awesomely"]
+    '''
+    def _split_with_maintain(self, value, treat_trailing_spaces_as_sentence = True, split_char_regex = STANDARD_REGEX):
+        result = []
+        check = value
+        
+        # compile regex
+        rx = re.compile(split_char_regex)
+        
+        # traverse the string
+        while len(check) > 0:
+            found  = rx.search(str(check))
+            if found == None:
+                result.append(check)
+                break
+            
+            idx = found.start()
+            result.append(str(check[:idx]))            # append the string
+            result.append(str(check[idx:idx+1]))    # append the puncutation so changing ? to . doesn't invalidate the whole sentence
+            check = check[idx + 1:]
+            
+            # group the trailing spaces if requested
+            if treat_trailing_spaces_as_sentence:
+                space_idx = 0
+                while True:
+                    if space_idx >= len(check):
+                        break
+                    if check[space_idx] != " ":
+                        break
+                    space_idx += 1
+                
+                if space_idx != 0:
+                    result.append(check[0:space_idx])
+            
+                check = check[space_idx:]
+            
+        return result
 
-		start = ""
-		end = ""
-		result = ""
-		
-		for diff in diffs:
-			op = diff.operation
-			if op == INSERTED:
-				start = "<ins>"
-				end = "</ins>"
-			elif op == DELETED:
-				start = "<del>"
-				end = "</del>"
-			else:
-				start = ""
-				end = ""
-			result += start + diff.line + end + "\n"
-			
-		return result
-
-	
-	'''
-	Apply a set of diffs to an original file to produce a new text file
-	'''
-	def apply_patch(self, doc, diffs):
-		if diffs == None:
-			return doc
-
-		splits = self._split_with_maintain(doc)
-		result = []
-		lastIndex = 0
-
-		for d in diffs:
-			# bring the original document up to speed
-			if d.start_index > lastIndex:
-				result += splits[lastIndex:d.start_index]
-			lastIndex += 1
-
-			# respond to the diff
-			if d.operation == DELETED:
-				lastIndex += d.length
-			elif d.operation == INSERTED:
-				result += d.line
-			else:
-				result += d.line
-				lastIndex += d.length
-
-		return ''.join(result)
-
-	'''
-	Reverse the direction of a patch
-	'''
-	def switch_patch_direction(self, diff):
-		result = []
-		for d in diff:
-			if d.operation == INSERTED:
-				new_op = DELETED
-			elif d.operation == DELETED:
-				new_op = INSERTED
-			else:
-				new_op = UNCHANGED
-
-			d.operation = new_op
-			result.append(DiffResult(d.start_index, d.length, d.line, new_op))
-		
-		return result
-
-	'''
-	Convenience method for removing a patch
-	'''
-	def remove_patch(self, doc, diffs):
-		patches = self.switch_patch_direction(diffs)
-		return self.apply_patch(doc, patches)
-
+    # packs up a set of results returned by DiffLib into a list of DiffResult objects
+    def _pack_results(self, raw_diffs):
+        results = []
+        
+        index = 0
+        # convert to DiffResult format
+        for d in raw_diffs:
+            if d[0] == '-':
+                op = DELETED
+                index += 1
+            elif d[0] == '+':
+                op = INSERTED
+            elif d[0] == '?':  # ignore informational lines
+                continue
+            else:
+                op = UNCHANGED
+                index += 1
+            results.append(DiffResult(index, 1, d[2:], op))
+        
+        return results
+        
+        
--- PyFreeDiffTests.py
+++ PyFreeDiffTests.py
@@ -25,13 +25,13 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 import PyFreeDiff
 import time
 
-class SpeedTests(object):
+class DiffEngineTests(object):
 	t1 = "This is a simple text. Lorem Ipsum etc. I want to iterate 10,000 times and count elapsed.  Some more matched text.  Some other unmatched text.  Finally some joy"
 	t2 = "This is a simple text. Lorem Ipsum etc. Some more matched text.  Some other changed text.  Finally some joy! There could be some more?  What do you think!? I don't know.  Its weird This thing that have with the guy in the lemon popsicle."
 
 	
 	# crudely tests 'n' iterations of the algorith... currently 1,000,000 takes ~270 seconds using
-	def multi_test(self, num_tests):
+	def diff_speed_test(self, num_tests):
 		start = time.time()
 		for i in range(num_tests):
 			sd = PyFreeDiff.DiffEngine()
@@ -41,3 +41,31 @@ class SpeedTests(object):
 		elapsed = time.time() - start
 		print str(num_tests) + " DiffEngine.diff() iterations took " + str(elapsed) + " seconds"
 		return elapsed, d, h
+
+	# test accuracy on some simple strings above, diffing, patching and removing patches
+	def diff_accuracy_test(self):
+		sd = PyFreeDiff.DiffEngine()
+		
+		print "Generating Diffs"
+		diffs = sd.diff(self.t1, self.t2)
+		for diff in diffs:
+			print "    >  [" + str(diff.operation) + "] " + diff.line
+		
+		print "Applying a patch to:"
+		print "    > " + self.t1
+		new_t2 = sd.apply_patch(self.t1, diffs)
+		print "Generated: "
+		print "    > " + new_t2
+		if self.t2 == new_t2:
+			print "Strings match!"
+		else:
+			print "Strings don't match!"
+		
+		print "Now removing the patch"
+		new_t1 = sd.remove_patch(new_t2, diffs)
+		print "Removing patch obtained: "
+		print "    > " + new_t1
+		if self.t1 == new_t1:
+			print "Strings match!"
+		else:
+			print "Strings don't match!"
