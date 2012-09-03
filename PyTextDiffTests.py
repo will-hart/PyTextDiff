8a907f5aa0c2b50ed95c34d683c685c2990fa096
William Hart <hart.wl@gmail.com>
2012-09-03 19:14:00 +0100

    Update unit tests for utilities

--- PyTextDiffTests.py
+++ PyTextDiffTests.py
@@ -22,24 +22,57 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTI
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
 
-import PyTextDiff
+from PyTextDiff import *
 import unittest
 
-class TestDiffGeneration(unittest.TestCase):
+class TestDiffUtilities(unittest.TestCase):
 
 	def setUp(self):
-		self.engine = PyTextDiff.DiffEngine()
-
-	
+		self.engine = DiffEngine()
+		
+		
 	def test_splitting_of_text(self):
 		easy_split_text = """This. Is an easy. splitting? function! it should return
 			at least 14 sections when split."""
-	
+			
 		result = self.engine._split_with_maintain(easy_split_text)
 		self.assertEqual(len(result), 14)		# correct number of splits
 		self.assertEqual(result[0], "This")		# correct first split
 		self.assertEqual(result[2], " ")		# correct space split
 		self.assertEqual(result[13], ".")		# correct last split
+		
+		
+	def test_packing_of_diffs_to_string(self):
+		pack_diff_text = "+ insert\n skip\n- remove\n+ insert 2"
+		expected_result = "+000@001:insert\n-001@001:remove\n+001@001:insert2"
+		result = self.engine._pack_results(pack_diff_text)
+		self.assertEquals(result, expected_result)
+		
+		
+	def test_empty_pack_returns_empty(self):
+		self.assertEquals(self.engine._pack_results(None), None)
+		self.assertEquals(self.engine._pack_results([]), None)
+		
+		
+	def test_unpacking_of_diffs_to_objects(self):
+		unpack_diff_text = "+000@001:insert\n-001@001:remove\n+001@001:insert2"
+		result = self.engine._unpack_results(unpack_diff_text)
+		
+		# check the individual diff objects
+		self.assertEquals(len(result), 3)
+		self.assertEquals(result[0].start_index, 0)
+		self.assertEquals(result[1].start_index, 1)
+		self.assertEquals(result[2].start_index, 1)
+		self.assertEquals(result[0].length, 1)
+		self.assertEquals(result[1].length, 1)
+		self.assertEquals(result[2].length, 1)
+		self.assertEquals(result[0].operation, "+")
+		self.assertEquals(result[1].operation, "-")
+		self.assertEquals(result[2].operation, "+")
+		self.assertEquals(result[0].line, "insert")
+		self.assertEquals(result[1].line, "remove")
+		self.assertEquals(result[2].line, "insert2")
+		
 		
 	def test_flattening_of_list(self):
 		input_list = [[1,2],[3],[4,5,6,7],[8,[9,10]]]
@@ -47,8 +80,8 @@ class TestDiffGeneration(unittest.TestCase):
 		
 		output = list(self.engine._flatten(input_list))
 		self.assertEquals(output, expected_output)
-
-
+		
+		
 if __name__ == '__main__':
 	unittest.main()	
 	
